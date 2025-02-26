62e9aac648a81d7db3c1a52bd39681403a30b62b
Ramon <91314507+ramonbeermann@users.noreply.github.com>
2025-02-26 07:27:42 +0100

    Update pirateBayCrawl.py
    "Refactored Pirate Bay crawler: Updated to modern Scrapy standards, improved error handling, added extra data (seeders, leechers, category, upload date), and integrated logging." 🚀

--- pirateBayCrawl.py
+++ pirateBayCrawl.py
@@ -1,32 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# author: Adastra
+# author: ramonbeermann (aktualisiert)
 
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.selector import HtmlXPathSelector
-from scrapy.item import Item, Field
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, Join
 
-class Torrent(Item):
-    url = Field()
-    tipo = Field()
-    torrentLink = Field()
-    size = Field()
-    description = Field()
+class Torrent(scrapy.Item):
+    url = scrapy.Field(output_processor=TakeFirst())
+    tipo = scrapy.Field(output_processor=TakeFirst())
+    torrentLink = scrapy.Field(output_processor=TakeFirst())
+    size = scrapy.Field(output_processor=TakeFirst())
+    description = scrapy.Field(output_processor=Join())
+    seeders = scrapy.Field(output_processor=TakeFirst())
+    leechers = scrapy.Field(output_processor=TakeFirst())
+    category = scrapy.Field(output_processor=TakeFirst())
+    upload_date = scrapy.Field(output_processor=TakeFirst())
 
 class PirateBaySpider(CrawlSpider):
-
-    name = 'thepiratebay.sx'
+    name = 'thepiratebay'
     allowed_domains = ['thepiratebay.sx']
     start_urls = ['http://thepiratebay.sx/browse']
-    rules = [Rule(SgmlLinkExtractor(allow=['/\d+']), 'parse_torrent')]
+    
+    rules = [Rule(LinkExtractor(allow=['/\d+']), callback='parse_torrent')]
 
     def parse_torrent(self, response):
-        x = HtmlXPathSelector(response)
-        torrent = Torrent()
-        torrent['url'] = response.url
-        torrent['tipo'] = x.select('//*[@id="searchResult"]/tr[1]/td[1]/center/a[2]//text()').extract()
-	torrent['torrentLink'] = x.select('//*[@id="searchResult"]/tr[1]/td[2]/a[2]/@href').extract()
-        torrent['description'] = x.select('//*[@id="searchResult"]/tr[1]/td[2]/div/a/@title').extract()
-        torrent['size'] = x.select('//*[@id="searchResult"]/tr[1]/td[2]/font/text()[2]').extract()
-        return torrent
+        self.logger.info(f"Scraping: {response.url}")
+        loader = ItemLoader(item=Torrent(), response=response)
+        
+        loader.add_value('url', response.url)
+        loader.add_xpath('tipo', '//*[@id="searchResult"]/tr[1]/td[1]/center/a[2]//text()')
+        loader.add_xpath('torrentLink', '//*[@id="searchResult"]/tr[1]/td[2]/a[2]/@href')
+        loader.add_xpath('description', '//*[@id="searchResult"]/tr[1]/td[2]/div/a/@title')
+        loader.add_xpath('size', '//*[@id="searchResult"]/tr[1]/td[2]/font/text()[2]')
+        loader.add_xpath('seeders', '//*[@id="searchResult"]/tr[1]/td[3]/text()')
+        loader.add_xpath('leechers', '//*[@id="searchResult"]/tr[1]/td[4]/text()')
+        loader.add_xpath('category', '//*[@id="searchResult"]/tr[1]/td[1]/center/a[1]/text()')
+        loader.add_xpath('upload_date', '//*[@id="searchResult"]/tr[1]/td[2]/font/text()[1]')
+
+        return loader.load_item()
